20a9592c512f9dc38294d5956c3ca71b0ce4d019
Mauricio Telles Silva Bichels <143547107+MauricioTSBichels@users.noreply.github.com>
2024-07-02 09:29:15 -0300

    Add files via upload

---
+++ botão fujão/js/index.js
@@ -0,0 +1,17 @@
+//alerta do botão sim
+function alertYes(){
+    alert("Ainda bem que vc tomou a decisão certa!")
+}
+
+function desvia(btn) {
+    // btn declarado na função
+    btn.style.position = 'absolute';
+    btn.style.bottom = geraPosicao(10, 90);
+    btn.style.left = geraPosicao(10, 90);
+    console.log('opa, desviei...');
+    
+}
+
+function geraPosicao(min, max) {
+    return (Math.random() * (max - min) + min) + "%";
+}
