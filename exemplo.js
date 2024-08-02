fa6debb703e237e44cf669f2418376c8b03c1e6c
Mauricio Telles Silva Bichels <143547107+MauricioTSBichels@users.noreply.github.com>
2024-08-02 11:17:49 -0300

    exemplo.js

---
+++ exemplo.js
@@ -0,0 +1,39 @@
+let primeiroNome = "Mauricio"
+let ultimoNome = "Bichels"
+
+let nomeCompleto = primeiroNome +" "+ ultimoNome
+console.log(nomeCompleto)
+
+//ex 2
+let numero1 = 10
+let numero2 = 56
+
+let soma = numero1 + numero2
+let sub = numero1 - numero2
+let multi = numero1 * numero2
+let divi = numero1 / numero2
+
+console.log(soma.sub,multi,divi)
+
+let estachovendo = true
+if(estachovendo) {
+  console.log("estachovendo")  
+}
+else {
+  console.log("não esta chovendo")  
+}
+//nota
+let resposta = null
+let nota
+console.log(resposta,nota)
+
+let idUnico = Symbol()
+let produto ={
+   [idUnico]: 123,
+
+   nome:"camiseta"
+}
+console.log(produto[idUnico],produto,nome)
+let numeroGrande = Bigint("123456789009876543212345678902678901234567890 ")
+let numero = 
+console.log(numerogrande,numero)
